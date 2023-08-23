de06438a224b971bd9a8900118c6b34a0146765f
Andrew Nesbitt <andrewnez@gmail.com>
2023-08-23 16:08:25 +0100

    open data exports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_161804) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_145213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_161804) do
     t.index ["package_id"], name: "index_dependencies_on_package_id"
     t.index ["package_name"], name: "index_dependencies_on_package_name"
     t.index ["version_id"], name: "index_dependencies_on_version_id"
+  end
+
+  create_table "exports", force: :cascade do |t|
+    t.string "date"
+    t.string "bucket_name"
+    t.integer "images_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "package_usages", force: :cascade do |t|
---
+++ lib/tasks/exports.rake
@@ -0,0 +1,8 @@
+namespace :exports do
+  desc 'Record export'
+  task record: :environment do
+    date = ENV['EXPORT_DATE'] || Date.today.strftime('%Y-%m-%d')
+    bucket_name = ENV['BUCKET_NAME'] || 'ecosystems-data'
+    Export.create!(date: date, bucket_name: bucket_name, images_count: Package.count)
+  end
+end
