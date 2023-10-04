e18f490fd798b2bed9d8f8c18ebeddd1baf2d9c7
Andrew Nesbitt <andrewnez@gmail.com>
2023-10-04 14:57:45 +0100

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_145213) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_105940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_145213) do
     t.string "description"
     t.bigint "downloads"
     t.string "repository_url"
+    t.string "status"
   end
 
   create_table "versions", force: :cascade do |t|
