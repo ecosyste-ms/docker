ef8fd1c426a37df70be66ffb75e3bbcb5a7d27d6
Andrew Nesbitt <andrewnez@gmail.com>
2024-03-24 21:06:47 +0000

    Add skip_before_action and after_action to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  skip_before_action :verify_authenticity_token
+
+  after_action lambda {
+    request.session_options[:skip] = true
+  }
+
   def default_url_options(options = {})
     Rails.env.production? ? { :protocol => "https" }.merge(options) : options
   end
