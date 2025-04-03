4efeeadaf061125fba06bd16759b9b73e3665101
Andrew Nesbitt <andrewnez@gmail.com>
2025-04-03 17:32:32 +0100

    Add search functionality to packages controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -8,6 +8,11 @@ class PackagesController < ApplicationController
     else
       scope = scope.order(Arel.sql(sort).desc.nulls_last)
     end
+
+    if params[:query].present?
+      query = "%#{params[:query].downcase}%"
+      scope = scope.where('LOWER(name) LIKE ? OR LOWER(description) LIKE ?', query, query)
+    end
     
     @pagy, @packages = pagy_countless(scope)
     fresh_when(@packages, public: true)
