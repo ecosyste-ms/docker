46fcb18b8b760aa8072aa4c7c966475b0283082a
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-02 17:05:58 +0100

    Set active_support.to_time_preserves_timezone to :zone

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Diff
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.exceptions_app = routes
+    config.active_support.to_time_preserves_timezone = :zone
 
     # Configuration for the application, engines, and railties goes here.
     #
