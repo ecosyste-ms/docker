f0c46fe1eb123c62e3979445acc9a666936de527
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-05 16:55:49 +0000

    Add statement_timeout override in migration

--- db/migrate/20251105142158_add_unique_index_to_versions.rb
+++ db/migrate/20251105142158_add_unique_index_to_versions.rb
@@ -1,5 +1,11 @@
 class AddUniqueIndexToVersions < ActiveRecord::Migration[8.0]
-  def change
+  def up
+    # Disable statement timeout for this long-running index creation
+    execute "SET statement_timeout = 0"
     add_index :versions, [:package_id, :number], unique: true
+  end
+
+  def down
+    remove_index :versions, [:package_id, :number]
   end
 end
