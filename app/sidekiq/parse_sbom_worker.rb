7d527fd3cd463b49f280201e52f9c69eec34e281
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-09 15:22:37 +0000

    Add lock options to ParseSbomWorker and SyncPackageWorker for improved concurrency control

--- app/sidekiq/parse_sbom_worker.rb
+++ app/sidekiq/parse_sbom_worker.rb
@@ -1,5 +1,6 @@
 class ParseSbomWorker
   include Sidekiq::Worker
+  sidekiq_options lock: :until_executed, lock_expiration: 20.minutes.to_i
 
   def perform(version_id)
     Version.find_by_id(version_id).try(:parse_sbom)
--- app/sidekiq/sync_package_worker.rb
+++ app/sidekiq/sync_package_worker.rb
@@ -1,5 +1,6 @@
 class SyncPackageWorker
   include Sidekiq::Worker
+  sidekiq_options lock: :until_executed, lock_expiration: 10.minutes.to_i
 
   def perform(package_id)
     Package.find_by_id(package_id).try(:sync)
